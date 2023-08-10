753599f2bb3fa26a9ca7e4d75dadc05e7e8e0a76
雷さん <admin@uplauncher.xyz>
2023-08-10 12:28:57 +0900

    fix

---
+++ include/include.js
@@ -0,0 +1,23 @@
+//includeHeader
+const headerRequest = new XMLHttpRequest();
+headerRequest.open("GET", "./include/header.html", true);
+headerRequest.onreadystatechange = function () {
+  if (headerRequest.readyState === 4 && headerRequest.status === 200) {
+    const response = headerRequest.responseText;
+    const element = document.querySelector("header");
+    element.innerHTML = response;
+  }
+};
+headerRequest.send();
+
+//includeFooter
+const footerRequest = new XMLHttpRequest();
+footerRequest.open("GET", "./include/footer.html", true);
+footerRequest.onreadystatechange = function () {
+  if (footerRequest.readyState === 4 && footerRequest.status === 200) {
+    const response = footerRequest.responseText;
+    const element = document.querySelector("footer");
+    element.innerHTML = response;
+  }
+};
+footerRequest.send();
