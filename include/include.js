ba83201fb2adefb7a21dedd1e1abcefeb474999f
雷さん <admin@uplauncher.xyz>
2023-08-10 15:47:36 +0900

    big update yes

--- include/include.js
+++ include/include.js
@@ -1,6 +1,6 @@
 //includeHeader
 const headerRequest = new XMLHttpRequest();
-headerRequest.open("GET", "./include/header.html", true);
+headerRequest.open("GET", "/include/header.html", true);
 headerRequest.onreadystatechange = function () {
   if (headerRequest.readyState === 4 && headerRequest.status === 200) {
     const response = headerRequest.responseText;
@@ -12,7 +12,7 @@ headerRequest.send();
 
 //includeFooter
 const footerRequest = new XMLHttpRequest();
-footerRequest.open("GET", "./include/footer.html", true);
+footerRequest.open("GET", "/include/footer.html", true);
 footerRequest.onreadystatechange = function () {
   if (footerRequest.readyState === 4 && footerRequest.status === 200) {
     const response = footerRequest.responseText;
